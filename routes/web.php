84dbb4cdb24ed8d42acd69fcac6b15131358b648
Kamil Staśko <kamil.stasko@fingoweb.com>
2025-04-30 16:15:22 +0200

    Remove unnecessary functionality and simplify package structure

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/speedtest', function () {
-    return view('speedtest::index');
-})->name('speedtest.index');
--- src/SpeedtestServiceProvider.php
+++ src/SpeedtestServiceProvider.php
@@ -8,48 +8,14 @@ use Illuminate\Support\ServiceProvider;
 class SpeedtestServiceProvider extends ServiceProvider
 {
     /**
-     * The package name.
-     *
-     * @var string
-     */
-
-    private string $name = 'speedtest';
-
-    /**
      * Register bindings in the container.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton($this->name, function ($app) {
-            return new SpeedtestManager();
-        });
-
         $this->commands([
             CreateSpeedtestSymlink::class,
         ]);
-    }
-
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadRoutesFrom(self::basePath('/routes/web.php'));
-        $this->loadViewsFrom(self::basePath('/resources/views'), $this->name);
-    }
-
-    /**
-     * Helper function to define the base path for package files.
-     *
-     * @param string $path
-     * @return string
-     */
-    public static function basePath(string $path): string
-    {
-        return __DIR__.'/..'.$path;
     }
 }
